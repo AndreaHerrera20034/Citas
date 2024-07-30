cedfd5c5e797b2fbe51556e919e3f58d797d96ad
AndreaHerrera20034 <andreaherrera20034@gmail.com>
2024-07-30 17:51:38 -0600

    ruta

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -66,7 +66,7 @@ const Navbar = () => {
             <ul className="py-2 font-medium" role="none">
               <li>
                 <a
-                  href="#"
+                  href="/Historial"
                   className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
                   role="menuitem"
                 >
